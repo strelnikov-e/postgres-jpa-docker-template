7a574bfc699daf33b41e978080f16e034a41c61a
strelnikov <evstrelnikov@gmail.com>
2024-02-08 00:15:50 -0500

    initial commit for PosgreSQL, Spring JPA app

---
+++ src/main/java/com/stelnikov/postgresjpa/PostgresJpaApplication.java
@@ -0,0 +1,13 @@
+package com.stelnikov.postgresjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stelnikov/postgresjpa/PostgresJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stelnikov.postgresjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
